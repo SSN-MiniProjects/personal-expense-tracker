36a65a842ce91264e2a386771ad47c018bfd0983
jagadish <goldjagadish@gmail.com>
2022-11-18 23:58:01 +0530

    changed sender mail

--- sendgrid_integration.py
+++ sendgrid_integration.py
@@ -9,8 +9,7 @@ from random import randint
 class SendGrid:
     def __init__(self):
         self.sg = SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
-        # self.from_email = "personalExpenseTracker7@gmail.com"
-        self.from_email = "abhisheknarayan19006@cse.ssn.edu.in"
+        self.from_email = "personalExpenseTracker7@gmail.com"
 
     def otp_generation(self):
         value = randint(1000, 10000)
