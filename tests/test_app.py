629bbd190a77e0cbb0600ef3ac667527a3a6e6d4
jagadishravichandran <goldjagadish@gmail.com>
2025-10-23 20:34:09 +0530

    deploy: add test container

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,6 +6,23 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app import app
 import pytest
 
+from testcontainers.postgres import PostgresContainer
+
+postgres = PostgresContainer("postgres:16-alpine")
+
+@pytest.fixture(scope="module", autouse=True)
+def setup(request):
+    postgres.start()
+
+    def remove_container():
+        postgres.stop()
+
+    request.addfinalizer(remove_container)
+    os.environ["DB_HOST"] = postgres.get_container_host_ip()
+    os.environ["DB_USERNAME"] = postgres.username
+    os.environ["DB_PASSWORD"] = postgres.password
+    os.environ["DB_NAME"] = postgres.dbname
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
